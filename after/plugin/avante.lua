b39de3debd327b615226b2b0e1907472e18e9fff
Horacio Hernandez <74020230+hernahoracio@users.noreply.github.com>
2026-02-13 10:33:20 -0600

    deleted comments

--- after/plugin/avante.lua
+++ after/plugin/avante.lua
@@ -4,10 +4,6 @@ require('avante').setup({
   behaviour = {
     auto_suggestions = false,
   },
-  -- You don't need a "providers = { copilot = ... }" block unless 
-  -- you want to change the specific model (e.g., to o1 or gpt-4o).
-  -- By default, it will use your Org's default model.
-
   mappings = {
     diff = {
       ours = "co",
